5db199402e9099ab92a3e409fdca15eed8cfa69a
Kenny <kguo6@my.bcit.ca>
2019-01-23 10:17:42 -0800

    added alert functionality to button array

--- comp4711/labs/2/buttons/buttons.js
+++ comp4711/labs/2/buttons/buttons.js
@@ -6,10 +6,12 @@ function createButtons(){
     for(let i = 0 ; i < 26; i++){
         let btn = document.createElement("BUTTON");
         let btnText = document.createTextNode(String.fromCharCode(value));
+        console.log(String.fromCharCode(value));
+        
         btn.appendChild(btnText);
         value++;
         btnCon.appendChild(btn);
-        btn.setAttribute("onClick", "btnClicked(this.name)");
+        btn.setAttribute("onClick", "btnClicked(this.innerText)");
         if(i === 12){
             btnCon = document.getElementById("row2");
         }
