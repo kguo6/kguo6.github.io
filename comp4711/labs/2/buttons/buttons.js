c59af86e5c5d29fefa755313e91a49841aa6865f
Kenny <kennyguoyuchen@hotmail.com>
2019-01-23 08:51:59 -0800

    work on website

---
+++ comp4711/labs/2/buttons/buttons.js
@@ -0,0 +1,23 @@
+
+function createButtons(){
+    let btnCon = document.getElementById("row1");
+
+    let value = "A".charCodeAt(0)
+    for(let i = 0 ; i < 26; i++){
+        let btn = document.createElement("BUTTON");
+        let btnText = document.createTextNode(String.fromCharCode(value));
+        btn.appendChild(btnText);
+        value++;
+        btnCon.appendChild(btn);
+        btn.setAttribute("onClick", "btnClicked(this.name)");
+        if(i === 12){
+            btnCon = document.getElementById("row2");
+        }
+    }
+}
+
+function btnClicked(value){
+    alert(value);
+}
+createButtons();
+
