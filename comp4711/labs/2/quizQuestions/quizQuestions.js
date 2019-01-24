933605c988b6475b5b2ab07ac7508036fd587079
Kenny <kguo6@my.bcit.ca>
2019-01-23 18:35:08 -0800

    fixed bug with validating input

--- comp4711/labs/2/quizQuestions/quizQuestions.js
+++ comp4711/labs/2/quizQuestions/quizQuestions.js
@@ -12,21 +12,22 @@ test = JSON.parse(result);
 
 
 //Converting to html
-var number = prompt("How many questions would you like to see? (0-5)");
+
 
 let questions = JSON.parse(result);
 
 let content = "";
-
-if(number <= questions.length && number >= 0){
-
-    for(let i = 0 ; i < number; i++){
-        content += questions[i];
-        content += "</br>"
+while(true){
+    var number = prompt("How many questions would you like to see? (0-5)");
+    if(number <= questions.length && number >= 0){
+        for(let i = 0 ; i < number; i++){
+            content += questions[i];
+            content += "</br>"
+        }
+        break;
+    } else {
+        alert("Not a valid input");
     }
-} else {
-    alert("Not a valid input");
-    number = prompt("How many questions would you like to see? (0-5)");
 }
 
 let con = document.getElementById("content");
